17851cfd7bc2844e8b6d86f34e8656c8f8ef1dac
Sergei-Siniuk <102694563+Sergei-Siniuk@users.noreply.github.com>
2022-11-08 22:59:05 +0300

    test

--- Yandex_praktikum_2.0/Z5_C.py
+++ Yandex_praktikum_2.0/Z5_C.py
@@ -1,12 +1,11 @@
 numbers_of_groups, numbers_of_audiences = map(int, input().split())
 number_of_students = list(map(int, input().split()))
-number_of_computers = [(j, i - 1) for i, j in zip(map(int, input().split()), range(1,numbers_of_audiences+1))]
+number_of_computers = [(j, i - 1) for i, j in zip(map(int, input().split()), range(1, numbers_of_audiences + 1))]
 sorted_number_of_computers = sorted(number_of_computers, key=lambda x: x[1])
-
 cabinet_list = []
 
 for i in number_of_students:
-    if i> sorted_number_of_computers[-1][1]:
+    if i > sorted_number_of_computers[-1][1]:
         cabinet_list.append(0)
         continue
     for j in sorted_number_of_computers:
@@ -17,11 +16,9 @@ for i in number_of_students:
     else:
         cabinet_list.append(0)
 
-
 if len(cabinet_list) == 0 or (len(cabinet_list) == 1 and 0 in cabinet_list):
     print(0)
     print(0)
 else:
-    print(len(list(filter(lambda x: x!= 0,cabinet_list))))
+    print(len(list(filter(lambda x: x != 0, cabinet_list))))
     print(*cabinet_list)
-print("TEST")
