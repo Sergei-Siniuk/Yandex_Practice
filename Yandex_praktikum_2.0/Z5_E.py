5bd35dbcca3221c833f77c5d74c6e9eba356acfe
Sergei-Siniuk <102694563+Sergei-Siniuk@users.noreply.github.com>
2022-11-17 13:16:15 +0300

    15.014(32lv)

--- Yandex_praktikum_2.0/Z5_E.py
+++ Yandex_praktikum_2.0/Z5_E.py
@@ -2,24 +2,24 @@ S = int(input())
 A = list(map(int, (input()).split()))[1:]
 B = list(map(int, (input()).split()))[1:]
 C = list(map(int, (input()).split()))[1:]
-a = []
-b = []
-for i in set(A):
-    a.append((i, A.index(i)))
-for i in set(B):
-    b.append((i, B.index(i)))
+
+break_ = False
+iter = 0
 
 
-for i in sorted(a,key=lambda x: x[1]):
-    if i[0] > S:
+for i_index, i_value in enumerate(A):
+    if i_value>S:
         continue
-    break_ = False
-    for j in sorted(b,key=lambda x: x[1]):
-        if (S - i[0] - j[0]) in set(C):
-            print(i[1], j[1], C.index(S - i[0] - j[0]))
+    for j_index, j_value in enumerate(B):
+        if i_value + j_value > S:
+            continue
+        iter+=1
+        if (S - i_value - j_value) in set(C):
+            print(i_index, j_index, C.index(S - i_value - j_value))
             break_ = True
             break
     if break_:
         break
 else:
     print("-1")
+print(iter)
